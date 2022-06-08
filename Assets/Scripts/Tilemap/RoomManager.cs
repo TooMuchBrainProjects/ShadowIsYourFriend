fbfa00d61c7e57f7702dead09d7e4aac9cbef566
Clemens Schlipfinger <clemens.schlipfinger@gmail.com>
2022-06-08 15:18:52 +0200

    GameScene recoverd

--- Assets/Scripts/Tilemap/RoomManager.cs
+++ Assets/Scripts/Tilemap/RoomManager.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System;
 using UnityEngine;
 
 public class RoomManager : MonoBehaviour
@@ -35,11 +35,11 @@ public class RoomManager : MonoBehaviour
 
     public void CreateNextRoom()
     {
-        IEnumerable<GameObject> matchingRooms = rooms.Where((r) => r.GetComponent<Room>().entryDoors == lastRoom.exitDoors).Where((r) => r.gameObject != lastRoomTemplate);
-        if(matchingRooms.Count() < 1)
+        GameObject[] matchingRooms = Array.FindAll(rooms, r => r.GetComponent<Room>().entryDoors == lastRoom.exitDoors && r.gameObject != lastRoomTemplate);
+        if(matchingRooms.Length < 1)
             Debug.LogError("No matching rooms available.");
 
-        GameObject nextRoom = matchingRooms.ElementAt(Random.Range(0, matchingRooms.Count()));
+        GameObject nextRoom = matchingRooms[UnityEngine.Random.Range(0, matchingRooms.Length)];
         lastRoomTemplate = nextRoom;
         GameObject instantiatedRoom = Instantiate(nextRoom, new Vector3(lastRoom.transform.localPosition.x + lastRoom.roomWidth / 2 + nextRoom.GetComponent<Room>().roomWidth / 2f, 0), Quaternion.identity, transform);
         activeRooms.AddLast(instantiatedRoom);
@@ -48,7 +48,7 @@ public class RoomManager : MonoBehaviour
 
     public void DestroyFirst()
     {
-        GameObject first = activeRooms.First();
+        GameObject first = activeRooms.First.Value;
         activeRooms.Remove(first);
         Destroy(first);
     }
