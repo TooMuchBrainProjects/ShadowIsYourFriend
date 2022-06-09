59bd6aaaba43c5db9932554b78e7fed8ca7c9c60
Programmierer281986 <f.schneider@htlkrems.at>
2022-06-09 15:44:28 +0200

    GameOver Screen fertig

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("GameScene");
     }
 
     public void QuitGame()
