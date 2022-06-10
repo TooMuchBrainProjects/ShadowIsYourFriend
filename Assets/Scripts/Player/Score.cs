5c7f6356c3865f43cab55a6d0e70dd64f7d6f531
Programmierer281986 <f.schneider@htlkrems.at>
2022-06-10 11:52:24 +0200

    yPosScoreDisappear Animations UI

--- Assets/Scripts/Player/Score.cs
+++ Assets/Scripts/Player/Score.cs
@@ -7,15 +7,19 @@ public class Score : MonoBehaviour
 {
     private double nextUpdate = 0.05;
     private bool playerIsDead = false;
+    private bool isOverYPos = false;
 
-    public Rigidbody2D rb;
     public double ScoreCounter;
     public int MaxScoreCounter;
     public double ScoreCounterSpeed;
     public int ScoreMinusPerSecond;
+    public double yPosScoreDisappear;
 
+    public Rigidbody2D rb;
     public TextMeshProUGUI CurrentScoreText;
     public TextMeshProUGUI HighscoreText;
+    public Animator scoreTransition;
+    public Animator attentionLevelTransition;
 
     [HideInInspector] private StealthMaster stealthMaster;
     [HideInInspector] public float PlayerDistance;
@@ -34,7 +38,6 @@ public class Score : MonoBehaviour
         // Zahl zwischen 0 und 1 machen (normalize)
         if(ScoreCounterSpeed >= 1)
             ScoreCounterSpeed = ScoreCounterSpeed / Mathf.Pow(10f, (float)ScoreCounterSpeed.ToString().Length);
-        //Debug.Log(ScoreCounterSpeed);
     }
 
     void Update()
@@ -42,15 +45,16 @@ public class Score : MonoBehaviour
         // If the next update is reached
         if (Time.time >= nextUpdate)
         {
-            //Debug.Log(Time.time + ">=" + nextUpdate);
             // Change the next update (current second+1)
             nextUpdate = Mathf.FloorToInt(Time.time) + 1;
             // Call your fonction
             UpdateEverySecond();
-        }
+        }        
 
         CurrentScoreText.text = CurrentScore.ToString();
         HighscoreText.text = Highscore.ToString();
+
+        ScoreTransition();
     }
 
     void UpdateEverySecond()
@@ -67,14 +71,26 @@ public class Score : MonoBehaviour
             ScoreCounter = MaxScoreCounter / (1 + ((MaxScoreCounter - ScoreCounter) / ScoreCounter) * Mathf.Pow((float)ScoreCounterSpeed, (float)ScoreCounter));
             if (Highscore < CurrentScore)
                 Highscore = CurrentScore;
-
-            //Debug.LogWarning("CurrentScore: " + CurrentScore);
-            //Debug.LogError("Highscore: " + Highscore);
         }
 
         if(stealthMaster.maxAttention > 50)
             stealthMaster.maxAttention -= 0.1f;
-        //Debug.Log(stealthMaster.maxAttention);
+    }
+
+    void ScoreTransition()
+    {
+        if(rb.position.y > yPosScoreDisappear && !isOverYPos)
+        {
+            scoreTransition.Play("ScoreHalfEnd");
+            attentionLevelTransition.Play("AttentionLevelHalfEnd");
+            isOverYPos = true;
+        }
+        else if(rb.position.y <= yPosScoreDisappear && isOverYPos)
+        {
+            scoreTransition.Play("ScoreHalfStart");
+            attentionLevelTransition.Play("AttentionLevelHalfStart");
+            isOverYPos = false;
+        }
     }
 
     public void ResetHighscore()
